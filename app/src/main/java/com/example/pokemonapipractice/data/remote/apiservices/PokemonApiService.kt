24167f14a4c72f142e7bf550cf8d1dae8e4019e9
adelwww <wsnezka@gmail.com>
2022-08-15 15:56:15 +0300

    add apiService

--- app/src/main/java/com/example/pokemonapipractice/data/remote/apiservices/PokemonApiService.kt
+++ app/src/main/java/com/example/pokemonapipractice/data/remote/apiservices/PokemonApiService.kt
@@ -1,4 +1,9 @@
 package com.example.pokemonapipractice.data.remote.apiservices
 
+import retrofit2.http.GET
+
 interface PokemonApiService {
+
+    @GET("pokemon")
+    suspend fun fetchPokemon()
 }
