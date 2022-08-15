6391ded221933e3556f6f03d518f219141e95c32
adelwww <wsnezka@gmail.com>
2022-08-15 15:55:46 +0300

    add retrofit

--- app/src/main/java/com/example/pokemonapipractice/data/remote/retrofit/NetworkClient.kt
+++ app/src/main/java/com/example/pokemonapipractice/data/remote/retrofit/NetworkClient.kt
@@ -1,4 +1,15 @@
 package com.example.pokemonapipractice.data.remote.retrofit
 
-class NetworkClient {
+import com.example.pokemonapipractice.data.remote.apiservices.PokemonApiService
+import javax.inject.Inject
+
+
+class NetworkClient @Inject constructor(
+    retrofitClient: RetrofitClient,
+    okHttp: OkHttp
+) {
+
+    private val provideRetrofit = retrofitClient.provideRetrofit(okHttp.provideOkHttpClient())
+
+    fun providePokemonApiService() = provideRetrofit.create(PokemonApiService::class.java)
 }
