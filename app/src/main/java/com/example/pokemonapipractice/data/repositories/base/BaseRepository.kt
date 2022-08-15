b579ccd521176bc1616bbfcabe4cf2d70912a8cc
adelwww <wsnezka@gmail.com>
2022-08-15 15:56:00 +0300

    add repository

--- app/src/main/java/com/example/pokemonapipractice/data/repositories/base/BaseRepository.kt
+++ app/src/main/java/com/example/pokemonapipractice/data/repositories/base/BaseRepository.kt
@@ -1,4 +1,24 @@
 package com.example.pokemonapipractice.data.repositories.base
 
-class BaseRepository {
+import android.util.Log
+import com.example.pokemonapipractice.utils.Either
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+abstract class BaseRepository {
+
+    protected fun <T> doRequest(
+        doSomethingInSuccess: ((T) -> Unit)? = null,
+        request: suspend () -> T
+    ) = flow<Either<String, T>> {
+        request().also { data ->
+            doSomethingInSuccess?.invoke(data)
+            emit(Either.Right(value = data))
+        }
+    }.flowOn(Dispatchers.IO).catch { exception ->
+        Log.e("RepositoryError", this@BaseRepository.javaClass.name, exception)
+        emit(Either.Left(value = exception.localizedMessage ?: "Error Occurred!"))
+    }
 }
