309d9fad550acb0f0b78577f21366ab280c6507f
ArzyKE <arzymatkadyrov968@gmail.com>
2022-08-15 18:16:32 -0700

    Displayed data

--- app/src/main/java/com/example/pokemonapipractice/ui/fragments/PokemonFragment.kt
+++ app/src/main/java/com/example/pokemonapipractice/ui/fragments/PokemonFragment.kt
@@ -1,9 +1,49 @@
 package com.example.pokemonapipractice.ui.fragments
 
-import androidx.fragment.app.Fragment
-import dagger.hilt.android.HiltAndroidApp
+import android.util.Log
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.pokemonapipractice.R
+import com.example.pokemonapipractice.databinding.FragmentPokemonBinding
+import com.example.pokemonapipractice.ui.adapters.PokemonAdapter
+import com.example.pokemonapipractice.ui.base.BaseFragment
+import com.example.pokemonapipractice.ui.state.UIState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-@HiltAndroidApp
-class PokemonFragment : Fragment() {
+@AndroidEntryPoint
+class PokemonFragment :
+    BaseFragment<PokemonViewModel, FragmentPokemonBinding>(R.layout.fragment_pokemon) {
 
+    override val binding by viewBinding(FragmentPokemonBinding::bind)
+    override val viewModel: PokemonViewModel by viewModels()
+    private val pokemonAdapter = PokemonAdapter()
+
+    override fun initialize() {
+        setupRecyclerview()
+    }
+
+    private fun setupRecyclerview() = with(binding.pokemonRecyclerview) {
+        layoutManager = LinearLayoutManager(requireContext())
+        adapter = pokemonAdapter
+    }
+
+    override fun setupObserves() {
+        lifecycleScope.launch {
+            viewModel.pokemonState.collect {
+                when (it) {
+                    is UIState.Error -> {
+                        Log.e("arzymat", it.error)
+                    }
+
+                    is UIState.Success -> {
+                        Log.e("anime", it.data.toString())
+                        pokemonAdapter.submitList(it.data)
+                    }
+                }
+            }
+        }
+    }
 }
