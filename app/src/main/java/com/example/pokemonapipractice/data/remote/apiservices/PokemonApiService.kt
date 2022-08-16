ea3b4bdde3c9028a517f3027548d4a87569d12c9
adelwww <wsnezka@gmail.com>
2022-08-16 17:15:08 +0300

    minor changes

--- app/src/main/java/com/example/pokemonapipractice/data/remote/apiservices/PokemonApiService.kt
+++ app/src/main/java/com/example/pokemonapipractice/data/remote/apiservices/PokemonApiService.kt
@@ -1,11 +1,11 @@
 package com.example.pokemonapipractice.data.remote.apiservices
 
-import com.example.pokemonapipractice.data.models.PokemonModel
-import com.example.pokemonapipractice.data.models.PokemonResponse
+import com.example.pokemonapipractice.data.dtos.PokemonDto
+import com.example.pokemonapipractice.data.dtos.PokemonResponseDto
 import retrofit2.http.GET
 
 interface PokemonApiService {
 
     @GET("pokemon")
-    suspend fun fetchPokemon() : PokemonResponse<PokemonModel>
+    suspend fun fetchPokemon() : PokemonResponseDto<PokemonDto>
 }
